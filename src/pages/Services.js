710ceb02ed72b641fbe7616c674e66054dfb8c9c
kalburgimanjunath <manjunathkalburgi65512@gmail.com>
2023-12-19 17:08:56 +0530

    services page added

--- src/pages/Services.js
+++ src/pages/Services.js
@@ -1,9 +1,12 @@
 export default function Services(){
     // List the services you offer with detailed descriptions.
     const services = ['Website Development','Product Design','Wireframes','Courses']
-    return <div><h3>Services</h3>
-    {/* <div>{services && services.map(item=>{
-    return <div>{item}</div>
-    })} */}</div>
+    return (
+         <div className="cards">
+            {services && services.map(item=>{
+                return <div>{item}</div>
+            })}
+         </div>
+    )
 
 }
