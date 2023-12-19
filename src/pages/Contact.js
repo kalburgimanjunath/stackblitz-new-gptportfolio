dcf7161b7f2c52a7341bad8dcee8ffd29fe3e7d8
kalburgimanjunath <manjunathkalburgi65512@gmail.com>
2023-12-19 13:45:53 +0530

    contact form aDDED

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,4 +1,18 @@
-Contact Information:
-Email, phone number, and location.
-Contact Form:
-Allow visitors to send you messages without revealing your email.
+// Contact Information:
+
+// Contact Form:
+// Allow visitors to send you messages without revealing your email.
+export default function Contact(){
+    return <div>
+        <h3>Contact Me</h3>
+        <div>Email:manjunathk85@gmail.com</div>
+        <div>Phone Number:9986407307</div>
+        <div>Address:189,Manjunath Nilaya,Ravi nagar,Gokul Road,hubli</div>
+        <div><h4>Contact Form:</h4>
+        <div>
+            <form></form>
+        </div>
+        </div></div>
+
+    </div>
+}
